f5f70a7ed612039e25f6aa9abd7255a1bcdad8a7
Danny Milosavljevic <dannym@scratchpost.org>
2024-01-17 22:54:03 +0100

    watchprotocol: Add comments for WatchListener.

--- app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/bluetooth/WatchListener.kt
+++ app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/bluetooth/WatchListener.kt
@@ -4,10 +4,13 @@ import com.friendly_machines.frbpdoctor.watchprotocol.notification.WatchRawRespo
 import com.friendly_machines.frbpdoctor.watchprotocol.notification.WatchResponse
 
 interface WatchListener {
+    /** Called with small responses that are complete in one call */
     fun onWatchResponse(response: WatchResponse)
+    /** Called with each chunk of a large transfer */
     fun onBigWatchRawResponse(rawResponse: WatchRawResponse) {
 
     }
+    /** Called as soon as our SetMtu request got a responds (which we send as soon as we have connected) */
     fun onMtuResponse(mtu: Int) {
 
     }
