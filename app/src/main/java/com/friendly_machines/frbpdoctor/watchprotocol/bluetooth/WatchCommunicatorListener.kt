1b5c516868cc6dc15b71722175f2d7fedd650ab3
Danny Milosavljevic <dannym@scratchpost.org>
2024-01-17 21:41:31 +0100

    watchprotocol: Clean up comments.

--- app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/bluetooth/WatchCommunicatorListener.kt
+++ app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/bluetooth/WatchCommunicatorListener.kt
@@ -4,9 +4,6 @@ import com.friendly_machines.frbpdoctor.watchprotocol.notification.WatchCommunic
 import com.friendly_machines.frbpdoctor.watchprotocol.notification.WatchResponse
 
 interface WatchCommunicatorListener {
-    //fun connectToDevice(deviceAddress: String)
-    //fun sendData(data: ByteArray)
-    // Other methods for Bluetooth action?
     fun onWatchResponse(response: WatchResponse)
     fun onBigWatchRawResponse(response: WatchCommunicationRawResponse)
     fun onMtuResponse(mtu: Int)
