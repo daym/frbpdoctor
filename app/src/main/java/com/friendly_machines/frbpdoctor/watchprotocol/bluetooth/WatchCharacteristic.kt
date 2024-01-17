2c2feac9f374ed764b48f3eadbe2f21d4a19a1b5
Danny Milosavljevic <dannym@scratchpost.org>
2024-01-17 22:05:24 +0100

    watchprotocol: Write all UUID strings the same way.

--- app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/bluetooth/WatchCharacteristic.kt
+++ app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/bluetooth/WatchCharacteristic.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 class WatchCharacteristic {
     companion object {
         val writingPortCharacteristic: UUID = UUID.fromString("00000001-0000-1001-8001-00805F9B07D0")
-        val notificationCharacteristic: UUID = UUID.fromString("00000002-0000-1001-8001-00805f9b07d0")
+        val notificationCharacteristic: UUID = UUID.fromString("00000002-0000-1001-8001-00805F9B07D0")
         val bigWritingPortCharacteristic: UUID = UUID.fromString("00000003-0000-1001-8001-00805F9B07D0")
         val bigNotificationCharacteristic: UUID = UUID.fromString("00000004-0000-1001-8001-00805F9B07D0")
     }
