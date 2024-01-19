af4d61e803c97236607f55dcf7683c9b852b2637
Danny Milosavljevic <dannym@scratchpost.org>
2024-01-19 21:07:03 +0100

    watchprotocol: Add hashCode and equals to AlarmDataBlock.

--- app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/notification/big/AlarmDataBlock.kt
+++ app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/notification/big/AlarmDataBlock.kt
@@ -1,4 +1,45 @@
 package com.friendly_machines.frbpdoctor.watchprotocol.notification.big
 
-class AlarmDataBlock {
+import java.nio.ByteBuffer
+
+// Monday first in repeats.
+data class AlarmDataBlock(val id: Int, val open: Byte, val hour: Byte, val min: Byte, val title: Byte, val repeats: ByteArray/*7*/) {
+    companion object {
+        fun parse(buf: ByteBuffer): AlarmDataBlock {
+            val id = buf.int
+            val open = buf.get()
+            val hour = buf.get()
+            val min = buf.get()
+            val title = buf.get()
+            val repeats = ByteArray(7)
+            buf.get(repeats)
+            return AlarmDataBlock(id = id, open = open, hour = hour, min = min, title = title, repeats = repeats)
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AlarmDataBlock
+
+        if (id != other.id) return false
+        if (open != other.open) return false
+        if (hour != other.hour) return false
+        if (min != other.min) return false
+        if (title != other.title) return false
+        if (!repeats.contentEquals(other.repeats)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + open
+        result = 31 * result + hour
+        result = 31 * result + min
+        result = 31 * result + title
+        result = 31 * result + repeats.contentHashCode()
+        return result
+    }
 }
