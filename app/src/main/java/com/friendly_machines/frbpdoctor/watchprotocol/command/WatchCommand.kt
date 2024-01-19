856c5ac3efd6b010de1d06e04f4a98300cca0d3c
Danny Milosavljevic <dannym@scratchpost.org>
2024-01-19 21:04:15 +0100

    watchprotocol: Use WatchOperation in WatchCommand

--- app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/command/WatchCommand.kt
+++ app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/command/WatchCommand.kt
@@ -1,11 +1,8 @@
 package com.friendly_machines.frbpdoctor.watchprotocol.command
-open class WatchCommand(val code: Short, val arguments: ByteArray) {
-    // TODO 1 OTA_REQUEST
-    // TODO 2 OTA_INFO
-    // TODO 3 OTA_FILE_OFFSET
-    // TODO 4 OTA_SEND_START
-    // TODO 5 OTA_SEND_FINISH
 
+import com.friendly_machines.frbpdoctor.watchprotocol.WatchOperation
+
+open class WatchCommand(val operation: WatchOperation, val arguments: ByteArray) {
     // TODO investigate 33
 
     // TODO 54 STEP_GOAL
