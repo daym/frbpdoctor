bc48ff90e551151826caffe22a108a69a398b369
Danny Milosavljevic <dannym@scratchpost.org>
2024-01-26 22:58:22 +0100

    watchprotocol: WatchSetMessageCommand: Update comments.

--- app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/command/WatchSetMessageCommand.kt
+++ app/src/main/java/com/friendly_machines/frbpdoctor/watchprotocol/command/WatchSetMessageCommand.kt
@@ -6,9 +6,6 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 class WatchSetMessageCommand(type: Byte, time: Int, title: ByteArray, content: ByteArray) : WatchCommand(WatchOperation.SetMessage, run {
-//    val title = ByteArray(0) // FIXME
-//    val content = ByteArray(0) // FIXME
-//    val type = 0.toByte() // FIXME 1 didn't work
     if (title.size > 120) {
         throw WatchMessageEncodingException("WatchSetMessageCommand title too long")
     }
@@ -17,6 +14,7 @@ class WatchSetMessageCommand(type: Byte, time: Int, title: ByteArray, content: B
     }
     val buf = ByteBuffer.allocate(1 + 4 + 2 + title.size + 2 + content.size).order(ByteOrder.BIG_ENDIAN)
     buf.put(type)
+    // I think for type == call, none of the other fields are necessary (title is used, though, if present)
     buf.putInt(time)
     buf.putShort(title.size.toShort())
     buf.put(title)
