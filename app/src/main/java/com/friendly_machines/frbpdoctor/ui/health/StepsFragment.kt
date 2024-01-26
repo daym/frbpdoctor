56d65a64a56aeb7b71b54418218849f0e81519fb
Danny Milosavljevic <dannym@scratchpost.org>
2024-01-27 00:29:15 +0100

    HealthActivity: StepsFragment: Provide dialog in order to set number of steps.

--- app/src/main/java/com/friendly_machines/frbpdoctor/ui/health/StepsFragment.kt
+++ app/src/main/java/com/friendly_machines/frbpdoctor/ui/health/StepsFragment.kt
@@ -1,17 +1,23 @@
 package com.friendly_machines.frbpdoctor.ui.health
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.friendly_machines.frbpdoctor.R
+import com.friendly_machines.frbpdoctor.WatchCommunicationClientShorthand
+import com.friendly_machines.frbpdoctor.watchprotocol.notification.WatchResponse
 import com.friendly_machines.frbpdoctor.watchprotocol.notification.big.StepsDataBlock
 
 class StepsFragment : Fragment() {
-
     private var recyclerView: RecyclerView? = null
 
     companion object {
@@ -19,10 +25,31 @@ class StepsFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_steps, container, false)
+        val view = inflater.inflate(R.layout.fragment_steps, container, false)
+        val setStepGoalButton = view.findViewById<Button>(R.id.setStepGoalButton)
+        setStepGoalButton.setOnClickListener {
+            val stepGoalEditText = EditText(requireContext())
+            stepGoalEditText.inputType = InputType.TYPE_CLASS_NUMBER
+            stepGoalEditText.hint = "Number of steps"
+
+            AlertDialog.Builder(requireContext()).setTitle("Your target number of steps").setMessage("What number of steps do you target?") // TODO translate
+                .setView(stepGoalEditText).setPositiveButton("Set Step Goal") { _, _ ->
+                    val stepGoalText = stepGoalEditText.text.toString()
+                    try {
+                        val stepGoal = Integer.parseInt(stepGoalText)
+                        WatchCommunicationClientShorthand.bindExecOneCommandUnbind(requireContext(), WatchResponse.SetStepGoal(0)) { binder ->
+                            binder.setStepGoal(stepGoal)
+                        }
+                    } catch (e: NumberFormatException) {
+                        Toast.makeText(requireContext(), "Number of steps wasn't a positive number", Toast.LENGTH_LONG).show()
+                    }
+                }
+                .setNegativeButton("Cancel") { _, _ -> }.show()
+        }
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,5 +69,4 @@ class StepsFragment : Fragment() {
         recyclerView!!.adapter = adapter
         //adapter.notifyDataSetChanged()
     }
-
 }
