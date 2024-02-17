026751599e9bbe066aa113b9d8a74b43bc4dea5f
Danny Milosavljevic <dannym@scratchpost.org>
2024-02-17 09:24:03 +0100

    idea: Update project.

--- fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchSSetSkinColorCommand.kt
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchSSetSkinColorCommand.kt
@@ -4,6 +4,9 @@ import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
 import com.friendly_machines.fr_yhe_pro.WatchOperation
 import java.nio.ByteBuffer
 
+/**
+ * TODO: 0: white; 1: yellowish, 2: yellow [the default!], 3: brownish, 4: brown, 5: black
+ */
 class WatchSSetSkinCommand(skin: Byte) : WatchCommand(WatchOperation.SSetSkin, byteArrayOf(skin)) {
     data class Response(val status: Byte) : WatchResponse() {
         companion object {
