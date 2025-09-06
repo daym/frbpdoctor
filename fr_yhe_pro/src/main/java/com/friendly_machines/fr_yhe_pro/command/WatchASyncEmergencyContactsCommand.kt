15f0ec6dd444a536906691aa38104c5d00757b64
Danny Milosavljevic <dannym@scratchpost.org>
2025-09-07 01:33:27 +0200

    Add questionable Command

---
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchASyncEmergencyContactsCommand.kt
@@ -0,0 +1,28 @@
+package com.friendly_machines.fr_yhe_pro.command
+
+import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
+import com.friendly_machines.fr_yhe_pro.WatchOperation
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class WatchASyncEmergencyContactsCommand(name: String, phone: String) : WatchCommand(WatchOperation.ASyncEmergencyContacts, run {
+    val nameBytes = name.toByteArray(Charsets.UTF_8)
+    val phoneBytes = phone.toByteArray(Charsets.UTF_8)
+    val totalSize = 1 + nameBytes.size + 1 + phoneBytes.size
+    val buffer = ByteBuffer.allocate(totalSize).order(ByteOrder.LITTLE_ENDIAN)
+    // FIXME: 0 terminate maybe ?
+    buffer.put(nameBytes.size.toByte())
+    buffer.put(nameBytes)
+    buffer.put(phoneBytes.size.toByte())
+    buffer.put(phoneBytes)
+    buffer.array()
+}) {
+    data class Response(val status: Byte) : WatchResponse() {
+        companion object {
+            fun parse(buf: ByteBuffer): Response {
+                val status = if (buf.hasRemaining()) buf.get() else 0
+                return Response(status = status)
+            }
+        }
+    }
+}
