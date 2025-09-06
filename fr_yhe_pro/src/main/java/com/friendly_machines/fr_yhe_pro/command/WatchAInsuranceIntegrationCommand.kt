caeb83c4d3967d0969fa439ca3302e8dd75af5da
Danny Milosavljevic <dannym@scratchpost.org>
2025-09-07 01:26:50 +0200

    Add some questionable unfinished commands

---
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchAInsuranceIntegrationCommand.kt
@@ -0,0 +1,30 @@
+package com.friendly_machines.fr_yhe_pro.command
+
+import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
+import com.friendly_machines.fr_yhe_pro.WatchOperation
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class WatchAInsuranceIntegrationCommand(policyType: Byte, planCode: Byte, coverageLevel: Byte, status: Byte, flags: Byte, message: String) : WatchCommand(WatchOperation.AInsuranceIntegration, run {
+    val messageBytes = message.toByteArray(Charsets.UTF_8)
+    val buf = ByteBuffer.allocate(5 + messageBytes.size).order(ByteOrder.LITTLE_ENDIAN)
+    buf.put(policyType)
+    buf.put(planCode)
+    buf.put(coverageLevel)
+    buf.put(status)
+    buf.put(flags)
+    buf.put(messageBytes)
+    buf.array()
+}) {
+    
+    data class Response(val result: Byte, val tpeResult: Byte?) : WatchResponse() {
+        companion object {
+            fun parse(buf: ByteBuffer): Response {
+                // FIXME names
+                val result = if (buf.hasRemaining()) buf.get() else 0
+                val tpeResult = if (buf.hasRemaining()) buf.get() else null
+                return Response(result = result, tpeResult = tpeResult)
+            }
+        }
+    }
+}
---
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchASetProductInfoCommand.kt
@@ -0,0 +1,26 @@
+package com.friendly_machines.fr_yhe_pro.command
+
+import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
+import com.friendly_machines.fr_yhe_pro.WatchOperation
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class WatchASetProductInfoCommand(typeCode: Byte, productInfo: String) : WatchCommand(WatchOperation.ASetProductInfo, run {
+    val infoBytes = productInfo.toByteArray(Charsets.UTF_8)
+    val data = ByteArray(1 + infoBytes.size)
+    data[0] = typeCode
+    infoBytes.copyInto(data, 1) // FIXME: 0-terminated ??
+    data
+}) {
+    data class Response(val status: Byte) : WatchResponse() {
+        companion object {
+            fun parse(buf: ByteBuffer): Response {
+                val status = if (buf.hasRemaining()) {
+                    buf.position(buf.limit() - 1)
+                    buf.get()
+                } else 0
+                return Response(status = status)
+            }
+        }
+    }
+}
