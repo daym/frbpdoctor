3cc7f614180f70d7a54c3fe485203080c34a492a
Danny Milosavljevic <dannym@scratchpost.org>
2025-09-06 20:51:26 +0200

    Add more commands

---
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchAControlAmbientLightCommand.kt
@@ -0,0 +1,24 @@
+package com.friendly_machines.fr_yhe_pro.command
+
+import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
+import com.friendly_machines.fr_yhe_pro.WatchOperation
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class WatchAControlAmbientLightCommand(controlValue: Byte) : WatchCommand(WatchOperation.AControlAmbientLight, run {
+    val buf = ByteBuffer.allocate(1).order(ByteOrder.LITTLE_ENDIAN)
+    buf.put(controlValue)
+    buf.array()
+}) {
+    
+    data class Response(val status: Byte) : WatchResponse() {
+        companion object {
+            fun parse(buf: ByteBuffer): Response {
+                val bytes = ByteArray(buf.remaining())
+                buf.get(bytes)
+                val status = if (bytes.isNotEmpty()) bytes.last() else 0
+                return Response(status = status)
+            }
+        }
+    }
+}
