08df4957230f9b1fe816b766b4b053f2b7006030
Danny Milosavljevic <dannym@scratchpost.org>
2025-09-07 19:15:11 +0200

    Add comment

--- fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchAGetRealData.kt
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchAGetRealData.kt
@@ -4,6 +4,7 @@ import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
 import com.friendly_machines.fr_yhe_pro.WatchOperation
 import java.nio.ByteBuffer
 
+// See indication.R*
 class WatchAGetRealData(sensorType: Byte, measureType: Byte /* 1...5 */, duration: Byte): WatchCommand(WatchOperation.ARealData, byteArrayOf(sensorType, measureType, duration)) {
 
     // FIXME: Should be real-time data streaming triger command.  No status response.
