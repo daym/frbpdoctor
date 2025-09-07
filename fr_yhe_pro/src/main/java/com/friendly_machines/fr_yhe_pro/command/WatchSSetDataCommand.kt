f86d0760164aba80cc227b1f749eab0704b073f5
Danny Milosavljevic <dannym@scratchpost.org>
2025-09-07 02:59:28 +0200

    Fix command parameters

--- fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchSSetDataCommand.kt
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchSSetDataCommand.kt
@@ -3,8 +3,16 @@ package com.friendly_machines.fr_yhe_pro.command
 import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
 import com.friendly_machines.fr_yhe_pro.WatchOperation
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
-class WatchSSetDataCommand(data1: String, data2: String, data3: String) : WatchCommand(WatchOperation.SSetData, byteArrayOf(*data1.toByteArray(), *data2.toByteArray(), *data3.toByteArray())) {
+class WatchSSetDataCommand(collectEnable: Byte, collectMode: Byte, collectInterval: Short, collectThreshold: Short) : WatchCommand(WatchOperation.SSetData, run {
+    val buf = ByteBuffer.allocate(6).order(ByteOrder.LITTLE_ENDIAN)
+    buf.put(collectEnable)
+    buf.put(collectMode)
+    buf.putShort(collectInterval)
+    buf.putShort(collectThreshold)
+    buf.array()
+}) {
     data class Response(val status: Byte, val data: Byte?) : WatchResponse() {
         companion object {
             fun parse(buf: ByteBuffer): Response {
