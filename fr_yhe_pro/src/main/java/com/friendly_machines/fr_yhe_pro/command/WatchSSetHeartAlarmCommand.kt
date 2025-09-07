1ea5b7087f831ff80092cb9325d400c2371b0277
Danny Milosavljevic <dannym@scratchpost.org>
2025-09-07 02:50:28 +0200

    Fix WatchSSetHeartAlarmCommand

--- fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchSSetHeartAlarmCommand.kt
+++ fr_yhe_pro/src/main/java/com/friendly_machines/fr_yhe_pro/command/WatchSSetHeartAlarmCommand.kt
@@ -4,9 +4,7 @@ import com.friendly_machines.fr_yhe_api.watchprotocol.WatchResponse
 import com.friendly_machines.fr_yhe_pro.WatchOperation
 import java.nio.ByteBuffer
 
-class WatchSSetHeartAlarmCommand(val enabled: Boolean, val maxValue: UByte) : WatchCommand(WatchOperation.SHeartAlarm, byteArrayOf(if (enabled) { 1 } else {
-    0
-}, maxValue.toByte(), 0)) {
+class WatchSSetHeartAlarmCommand(val enabled: Byte, val minValue: Byte, val maxValue: Byte) : WatchCommand(WatchOperation.SHeartAlarm, byteArrayOf(enabled, minValue, maxValue)) {
     data class Response(val status: Byte, val data: Byte?) : WatchResponse() {
         companion object {
             fun parse(buf: ByteBuffer): Response {
